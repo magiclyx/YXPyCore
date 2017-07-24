5c15946c264d4527b4c1b38a9de46b8e6bcf0892
yuxi <liu.yuxi.canaan@gmail.com>
2017-07-25 02:09:19 +0800

    [comments] 增强loader函数 [jira_id] none [version] master

--- yxcore/__init__.py
+++ yxcore/__init__.py
@@ -4,3 +4,23 @@ from __future__ import unicode_literals
 
 
 __author__ = 'yuxi'
+
+try:
+    import six
+except ImportError as e:
+    from .utility import loader
+    import os
+
+    print('error: 无法找到six模块，尝试使用自带机制')
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'vendors/six')
+    loader.load(path)
+
+try:
+    import backports.inspect
+except ImportError as e:
+    from .utility import loader
+    import os
+
+    print('error: 无法找到backports.inspect模块，尝试使用自带机制')
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'vendors/backports.inspect')
+    module = loader.load(path)
--- yxcore/utility/loader.py
+++ yxcore/utility/loader.py
@@ -3,16 +3,135 @@
 from __future__ import unicode_literals
 import sys
 import importlib
+# import six
+import os
 
 
 __author__ = 'yuxi'
 
 
-# module_name 指定加载后的名字。如果未指定，则使用path作为module_name
 def load(path, module_name=None):
-    lib = importlib.import_module(path)
-    sys.modules[module_name if module_name is not None else path] = lib
-    return lib
+
+    def _py2_check_magic_num(cache_path):
+        """
+        验证编译后文件的magic number , 是否和当前python解释器兼容
+        """
+        import imp
+        if os.access(cache_path, os.F_OK) and os.access(cache_path, os.R_OK):
+            with open(cache_path, 'r') as f:
+                magic = f.read(4)
+                if magic is not None:
+                    return True if magic == imp.get_magic() else False
+
+    def _fixed_cache_from_source(source_path):
+        """
+        根据python文件，获取编译后缓存文件的位置
+        """
+        if sys.version_info[0] is 2:
+            import imp
+            cache_path = source_path + '.pyc'
+            if _py2_check_magic_num(cache_path) is True:
+                return cache_path
+
+            cache_path = source_path + '.pyo'
+            if _py2_check_magic_num(cache_path) is True:
+                return cache_path
+
+            return None
+        elif sys.version_info[0] is 3:
+            import importlib.util
+            # 实际上，cache_from_source 是需要.py扩展名的, 上面是为了处理方便. 这部分可以考虑重写
+            return importlib.util.cache_from_source(source_path + '.py')
+        else:
+            raise ImportError('not implement in python %d.%d.%d' % (sys.version_info[0:3]))
+
+    def _fixed_load_compiled(load_module_name, load_module_path):
+        """
+        加载编译后的文件
+        """
+        if sys.version_info[0] == 2:
+            import imp
+            return imp.load_compiled(load_module_name, load_module_path)
+        elif sys.version_info[0] is 3:
+            import importlib
+            if sys.version_info[0:2] > (3, 4):
+                import importlib.util
+                spec = importlib.util.spec_from_file_location(load_module_name, load_module_path)
+                result_module = importlib.util.module_from_spec(spec)
+                spec.loader.exec_module(result_module)
+                sys.modules[load_module_name] = result_module
+                return result_module
+            else:
+                import importlib.machinery
+                return importlib.machinery.SourcelessFileLoader(load_module_name, load_module_path).load_module()
+
+    def _fixed_load_normal(load_module_name, load_module_path):
+        """
+        加载源码文件
+        """
+        if sys.version_info[0] == 2:
+            import imp
+            return imp.load_source(load_module_name, load_module_path)
+        elif sys.version_info[0] is 3:
+            if sys.version_info[0:2] > (3, 4):
+                import importlib.util
+                spec = importlib.util.spec_from_file_location(load_module_name, load_module_path)
+                result_module = importlib.util.module_from_spec(spec)
+                spec.loader.exec_module(result_module)
+                sys.modules[load_module_name] = result_module
+                return result_module
+            else:
+                import importlib.machinery
+                return importlib.machinery.SourceFileLoader(load_module_name, load_module_path).load_module()
+
+    def _try_load_with_no_ext_path(try_module_name, try_path):
+        """
+        尝试加载已经去除扩展名的python模块
+        """
+
+        result_module = None
+
+        guess_path = _fixed_cache_from_source(try_path)
+        if guess_path is not None and os.access(guess_path, os.F_OK) and os.access(guess_path, os.R_OK):
+            result_module = _fixed_load_compiled(try_module_name, guess_path)
+
+        guess_path = try_path + '.py'
+        if guess_path is not None and result_module is None and os.access(guess_path, os.F_OK) and os.access(guess_path, os.R_OK):
+            result_module = _fixed_load_normal(try_module_name, guess_path)
+
+        return result_module
+
+    # 去除扩展名
+    no_ext_path = path
+    if path.endswith('.py') or path.endswith('.pyc'):
+        no_ext_path, _ = os.path.splitext(path)
+
+    # 获得父文件夹 以及 包信息
+    parent_path, package_info = os.path.split(no_ext_path)
+
+    # 访问沿途的所有包(访问包的 __init__)
+    package_path = parent_path
+    current_package_info = None
+    package_list = package_info.split('.')
+    for package_name in package_list:
+
+        if current_package_info is None:
+            current_package_info = package_name
+        else:
+            current_package_info = '.'.join([current_package_info, package_name])
+
+        package_path = os.path.join(package_path, package_name)
+        module = _try_load_with_no_ext_path(current_package_info, package_path)
+        if module is None:
+            module = _try_load_with_no_ext_path(current_package_info, os.path.join(package_path, '__init__'))
+
+        if module is None:
+            raise ImportError('无效包路径 %s' % (path, ))
+
+    if module is not None and module_name is not None:
+        sys.modules[module_name] = module
+
+    return module
 
 
 def module_by_path(path):
