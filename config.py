c40c117a45dcc76b3599c7af4a916484edda8474
Yuxi Liu <liu.yuxi.canaan@gmail.com>
2018-12-25 01:27:49 +0800

    [comments] 简单例子 [jira_id] none [version] master

---
+++ config.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import os
+import argparse
+from yxcore.cmdline import argument
+
+__author__ = 'yuxi'
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SITE_DIR = os.path.dirname(__file__)
+
+
+# debug 模式
+DEBUG = True
+
+
+YXAPPLICATION = {
+    'app_name': None,
+    'version': '0.0.1',
+}
+
+
+# 安装的库文件
+YXINSTALLED_LIBS = [
+]
+
+
+YXCOMMANDLINE = {
+
+    'description': '工程分析工具, 使用 %(prog)s command --help 获得指定子命令的信息',
+    'formatter_class': argparse.RawDescriptionHelpFormatter,
+    'commandline_entry': 'example.entry',
+    'commandline_define': [
+        argument('--workspace', help='指定workspace', type=str, required=True),
+        argument('--scheme', help='指定scheme', type=str, required=True),
+    ]
+
+}
+
+# TODO loger 中支持 DEBUG 和 非DEBUG 模式的配置
+YXLOGGER = {
+
+    # default setting name
+    'default_log_identifier': 'default_logger',
+
+    # logger
+    'loggers': {
+        'default_logger': {
+            'handlers': ['console'],
+            'propagate': False,
+            'level': 'DEBUG',
+        },
+    },
+
+}
+
+
+# LINKMAP_DUMP_FILE = '/Users/yuxi/python/YXProjectAnalysis/log/linkmap'
+
---
+++ example.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python -00
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import os
+import sys
+
+
+__author__ = 'yuxi'
+
+
+if __name__ == "__main__":
+    os.environ.setdefault("YXCORE_SETTING_MODULE", "config")
+
+    from yxcore.application import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
+
+
+def entry(cmdline):
+
+    from yxcore import logger
+
+    print(cmdline.workspace)
+    print(cmdline.scheme)
+
+    logger.warning('finished')
+
+
+
